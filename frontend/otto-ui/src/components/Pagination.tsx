cec304e62de9039cb3fe9527ac3bfcdcd915643b
Suren Nikoghosyan <snikoghosyan@vmware.com>
2023-08-10 16:44:26 +0400

    - Pagination imrovements.

--- frontend/otto-ui/src/components/Pagination.tsx
+++ frontend/otto-ui/src/components/Pagination.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { Dispatch } from 'react';
 import Pagination from 'react-bootstrap/Pagination';
-import { useSelector, useDispatch } from "react-redux";
-
-import { RootState } from "../store";
+import { useDispatch } from "react-redux";
 import productSlice from '../store/productSlice';
+import { MIN_PAGE_NUMBER_BEFORE_AFTER_ELLIPSIS } from '../constants'
 
 import "../styles/pagination.css";
+import { number } from 'ra-core';
+import { AnyAction } from 'redux';
+
+const isPageRendered = (totalPages: number, selectedPage: number, consideredPage: number) => {
+  if((consideredPage <= 2 || (totalPages - consideredPage) < MIN_PAGE_NUMBER_BEFORE_AFTER_ELLIPSIS) ||
+    Math.abs(selectedPage-consideredPage) <=1){
+    return true;
+  }
+  return false;
+  
+}
+
+const getPages = (totalPages: number, selectedPage: number, dispatch: Dispatch<AnyAction>) => {
+  const componentArray: any[] = [];
+  let beforeAfterEllipse = false;
+  for (let i = 1; i <= totalPages; ++i) {
+    if(isPageRendered(totalPages, selectedPage, i)){
+      beforeAfterEllipse = false;
+      componentArray.push((i) === selectedPage ? 
+      <Pagination.Item active>{i}</Pagination.Item> :
+      <Pagination.Item onClick={() => {
+        dispatch(productSlice.actions.setPage(i))
+        }}>{i}</Pagination.Item>)
+    } else if(!beforeAfterEllipse){
+      beforeAfterEllipse = true;
+      componentArray.push(<Pagination.Ellipsis />);
+    }
+  }
+  return componentArray;
+}
 
 function PaginationExample(totalPages: number, selectedPage: number) {
   const dispatch = useDispatch();
-
+  const arr: number[] = [...Array(totalPages + 1)];
   return (
     <Pagination>
       <Pagination.First onClick = {() => {
@@ -19,17 +48,7 @@ function PaginationExample(totalPages: number, selectedPage: number) {
         dispatch(productSlice.actions.setPage(selectedPage - 1))
       }}/>
     
-      <Pagination.Ellipsis />
-
-      {[...Array(totalPages + 1)].map((x, i) =>
-          i === 0 ? <></> : 
-          (i) === selectedPage ? 
-          <Pagination.Item active>{i}</Pagination.Item> :
-          <Pagination.Item onClick={() => {
-            dispatch(productSlice.actions.setPage(i))
-            }}>{i}</Pagination.Item>
-      )}
-      <Pagination.Ellipsis />
+      {getPages(totalPages, selectedPage, dispatch)}
       <Pagination.Next onClick = {() => {
         if(selectedPage !== totalPages) {
           dispatch(productSlice.actions.setPage(selectedPage + 1))
--- frontend/otto-ui/src/constants/index.tsx
+++ frontend/otto-ui/src/constants/index.tsx
@@ -1,2 +1,4 @@
 export const API_URL = "http://localhost:8080"
 export const ITEM_PER_PAGE: number = 4;
+export const MIN_PAGE_NUMBER_BEFORE_AFTER_ELLIPSIS = 2;
+export const MIN_PAGE_NUMBER_BEFORE_AFTER_SELECTED_PAGE = 1;
